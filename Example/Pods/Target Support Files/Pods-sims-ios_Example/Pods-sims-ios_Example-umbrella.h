5bae7c20c4d81fd6bec313a1f21597a3f091c24a
John Nilsen <nilsen340@gmail.com>
2016-05-23 14:29:24 -0400

    Configure podspec for TapglueSims

---
+++ Example/Pods/Target Support Files/Pods-sims-ios_Example/Pods-sims-ios_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_sims_ios_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_sims_ios_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/TapglueSims/TapglueSims-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TapglueSims : NSObject
+@end
+@implementation PodsDummy_TapglueSims
+@end
